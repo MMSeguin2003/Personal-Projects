93ae1eafc32a6a715f731728e1fd5580a7a5aa1b
Matthew Seguin <mmseguin2003@berkeley.edu>
2024-11-06 12:37:20 -0800

    Refining class, adding method call in __init__

--- First n Primes.py
+++ First n Primes.py
@@ -1,15 +1,23 @@
-n = input("Enter a nonnegative integer n to get the first n primes.\n")
+n = input("Enter a non-negative integer n to get the first n primes.\n")
 
 class primes:
-    def __init__(self):
+    def __init__(self, n):
         self.primes = [2]
+        self.first_n_primes(n = n)
     
     def primality(self, n):
         for j in self.primes:
             if n%j == 0:
                 return False
-            elif j*j > n:
+            elif j**2 > n:
                 return True
+
+    def __str__(self):
+        k = 1
+        for j in self.primes:
+            print(f"{k}: " + str(j))
+            k += 1
+        return(", ".join([str(i) for i in self.primes]))
 
     def first_n_primes(self, n):
         if n == 0:
@@ -21,16 +29,12 @@ class primes:
         else:
             j = 3
             k = 2
-            print("1: " + str(self.primes[-1]))
             while len(self.primes) < n:
                 if self.primality(j):
                     self.primes.append(j)
-                    print(str(k) + ": " + str(self.primes[-1]))
                     k += 1
                 j += 2
-            return self.primes
+            return(self.primes)
 
-list = primes()
-t = list.first_n_primes(float(n))
-if not t:
-    print(t)
+lst = primes(int(n))
+print(lst)
